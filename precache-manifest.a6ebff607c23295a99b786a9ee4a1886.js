95e34dcf372d04a3998a440e85e6d28f0fa5bcc4
Alan Casas <alancasasarevalo@gmail.com>
2019-04-06 19:11:25 +0200

    first commit

---
+++ precache-manifest.a6ebff607c23295a99b786a9ee4a1886.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9e3ed2b5f79147291400",
+    "url": "/static/js/main.412d0d71.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "9e3ed2b5f79147291400",
+    "url": "/static/css/main.580f221a.chunk.css"
+  },
+  {
+    "revision": "0d1a605ef4e24f6f85b0b6bfd997be4d",
+    "url": "/index.html"
+  }
+];
